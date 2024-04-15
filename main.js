5db97f8781304377c76c9d2ec5abbb974e8e375b
Nicollas Saar <107404703+nicrinhareidelas@users.noreply.github.com>
2024-04-15 09:03:10 -0300

    .

--- main.js
+++ main.js
@@ -5,14 +5,14 @@ function verificarTime() {
 
   while (continuarPerguntando) {
     
-    var respostaTime = prompt("Qual o melhor time?");
+    var respostaTime = prompt("Qual o professor?");
 
     
-    if (respostaTime.toLowerCase() === "athlético pr") {
-      alert("Isso mesmo! O Athlético PR é o melhor time!");
+    if (respostaTime.toLowerCase() === "christian") {
+      alert("Isso mesmo! O professor christian é o melhor professor");
       continuarPerguntando = false; // Sai do loop após a resposta correta
     } else {
-      alert("Não é isso! Tente novamente.");
+      alert("Não é isso!Dica:ele dá aula de português. Tente novamente.");
     }
   }
 }
